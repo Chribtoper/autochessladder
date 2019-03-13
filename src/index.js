21285b56eda8edf3083b7c7e128b24b916cc9180
Chribtoper <xcstolfax@gmail.com>
2019-03-13 19:54:28 -0400

    added some more packages. Preparing to setup a Redux environment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import UsersReducer from './reducers/UsersReducer.js'
+import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ UsersReducer: UsersReducer })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('root')
+  )
+
 serviceWorker.unregister();
