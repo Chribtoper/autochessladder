b018eda02b6164ef3f163fc518ea330031f90eb9
Chribtoper <xcstolfax@gmail.com>
2019-03-17 01:01:03 -0400

    Removed junk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Fragment, Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Redirect, Switch, withRouter, Link } from 'react-router-dom'
+import { Header, Image, Container, Grid, Divider, Button, Form, Segment, Message } from 'semantic-ui-react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+
+  }
+
+  handleClick = () => {
+    axios.get(`http://localhost:8080/account`)
+      .then(r => {
+        debugger
+      })
+  }
+
   render() {
+    // <Button onClick={()=>this.handleClick()}>Sign In</Button>
     return (
-      <div>Hello World!</div>
+      <>
+        <Button href='http://localhost:8080/auth/steam'>login</Button>
+        <Button onClick={()=>this.handleClick()}>Account</Button>
+      </>
     );
   }
 }
